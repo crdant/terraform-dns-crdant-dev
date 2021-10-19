682e1456cbce59b6a47cb6ac84a205634c6adde7
Chuck D'Antonio <cdantonio@pivotal.io>
2021-10-19 10:18:32 -0400

    Uses `GOOGLE_CREDENTIALS` variable to simplify provider config

--- src/terraform/providers.tf
+++ src/terraform/providers.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  credentials = fileexists(var.gcp_key) ? file(var.gcp_key) : var.gcp_key
   project     = var.project
 }
 
--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -2,10 +2,6 @@ variable "project" {
   type = string
 }
 
-variable "gcp_key" {
-  type = string
-}
-
 variable "region" {
   type = string
 }
